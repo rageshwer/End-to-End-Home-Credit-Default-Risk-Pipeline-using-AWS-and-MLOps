1e70befb2370944adda0e01a61669071c6e90778
rageshwer <rageshwersinghsingh@gmail.com>
2026-06-20 00:22:59 +0530

    SHAP added

--- tests/test_training.py
+++ tests/test_training.py
@@ -5,7 +5,7 @@ from src.training.train_save_model import prepare_train_data,train_cv
 def test_train_save():
     train=pd.read_parquet('tests/mock_data/processed/final_train.parquet')
     _,_,cat_cols=prepare_train_data(train,'TARGET')
-    model,feature_importance_mean,_ = train_cv(train,cat_cols,target='TARGET',ci_mode=True)
+    model,feature_importance_mean,_,trees = train_cv(train,cat_cols,target='TARGET',ci_mode=True)
 
     assert model is not None, "Model failed to instantiate and train."
     assert isinstance(feature_importance_mean, pd.DataFrame), "Feature importance object must be a pandas DataFrame."
